39a5751a676377b3cd8016b0f9f88f03125f5b56
Yann <227698+yanncarlier@users.noreply.github.com>
2025-11-25 11:51:41 +0700

    added ERC20 Contract

---
+++ Contract/Ethereum-tx.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "./@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./@openzeppelin/contracts/security/Pausable.sol";
+import "./@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact security@ethereum-tx.com
+contract EthereumTx is ERC20, ERC20Burnable, Pausable, Ownable {
+    constructor() ERC20("Ethereum-tx", "ETX") {
+        _mint(msg.sender, 21000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
